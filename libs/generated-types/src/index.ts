256b43f00616594e5f430861f35b6dcb2280b2fd
Abe Fehr <afehr@wealthsimple.com>
2025-05-01 15:26:37 -0400

    Initial commit

---
+++ libs/generated-types/src/index.ts
@@ -0,0 +1,5 @@
+// Changing this file shouldn't invalidate the cache, but it does.
+// But only when @nx/js is installed!
+export type FakeGQLType = number | string;
+
+// asdf
---
+++ libs/my-lib/src/index.ts
@@ -0,0 +1,5 @@
+import { type FakeGQLType } from '@company/generated-types';
+
+export const fakeGQLType: FakeGQLType = 'test';
+
+console.log(fakeGQLType);
